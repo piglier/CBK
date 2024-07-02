551c9eeed85cb68475e1ccd2d3ef0f15484be5e9
JasonZhu <JasonZhu@taiwanmobile.com>
2024-07-02 11:27:27 +0800

    Initial Commit

---
+++ CathayBKHK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CathayBKHK
+//
+//  Created by 朱彥睿 on 2024/7/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
